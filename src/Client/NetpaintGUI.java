e8d6c8cf90eda801a945cabaab30ac36750a1605
tfasulo <tfasulo@email.arizona.edu>
2014-10-21 23:02:35 -0700

    changes

--- src/Client/NetpaintGUI.java
+++ src/Client/NetpaintGUI.java
@@ -3,6 +3,7 @@ package Client;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
@@ -21,6 +22,7 @@ import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
+import Shapes.Oval;
 import Shapes.Rectangle;
 
 public class NetpaintGUI extends JFrame{
@@ -92,6 +94,10 @@ public class NetpaintGUI extends JFrame{
 		private int pivotX;
 		private int pivotY;
 		private boolean isDrawing;
+		int width;
+		int height;
+		int upperX;
+		int upperY;
 		
 		public DrawingArea() {
 			
@@ -117,23 +123,70 @@ public class NetpaintGUI extends JFrame{
 				newX = arg0.getX();
 				newY = arg0.getY();
 				
+				
 				if(isDrawing){
 					if(rectangle.isSelected()){
-						
-						if(newX-pivotX<0 && newY-pivotY<0){
+						if(newX-pivotX<=0 && newY-pivotY<=0){
 							Graphics2D g2 = (Graphics2D) getGraphics();
-							Rectangle2D.Double rect = new Rectangle2D.Double(newX-pivotX, newY-pivotY, pivotX, pivotY);
+							Rectangle2D.Double rect = new Rectangle2D.Double(newX, newY, pivotX-newX, pivotY-newY);
 							g2.fill(rect);
 						}
+						else if (newX-pivotX<0 && newY-pivotY>0){
+							Graphics2D g2 = (Graphics2D) getGraphics();
+							Rectangle2D.Double rect = new Rectangle2D.Double(newX, pivotY, pivotX-newX, newY-pivotY);
+							g2.fill(rect);
+						}
+						
+						else if (newX-pivotX>0 && newY-pivotY<0){
+							Graphics2D g2 = (Graphics2D) getGraphics();
+							Rectangle2D.Double rect = new Rectangle2D.Double(pivotX, newY, newX-pivotX, pivotY-newY);
+							g2.fill(rect);
+						}
+						
 						else{
 							Graphics2D g2 = (Graphics2D) getGraphics();
 							Rectangle2D.Double rect = new Rectangle2D.Double(pivotX, pivotY, newX-pivotX, newY-pivotY);
 							g2.fill(rect);
 						}
-						
 					}
 					else if(oval.isSelected()){
+						if(newX-pivotX<=0 && newY-pivotY<=0){
+
+							width = pivotX-newX;
+							height = pivotY-newY;
+							upperX = newX;
+							upperY = newY;
+//							Graphics2D g2 = (Graphics2D) getGraphics();
+//							g2.fillOval(newX, newY, pivotX-newX, pivotY-newY);
+						}
+						else if (newX-pivotX<0 && newY-pivotY>0){
+							width = pivotX-newX;
+							height = newY-pivotY;
+							upperX = newX;
+							upperY = pivotY;
+//							Graphics2D g2 = (Graphics2D) getGraphics();
+//							g2.fillOval(newX, pivotY, pivotX-newX, newY-pivotY);
+						}
 						
+						else if (newX-pivotX>0 && newY-pivotY<0){
+							width = newX-pivotX;
+							height = pivotY-newY;
+							upperX = pivotX;
+							upperY = newY;
+//							Graphics2D g2 = (Graphics2D) getGraphics();
+//							g2.fillOval(pivotX, newY, newX-pivotX, pivotY-newY);
+						}
+						
+						else{
+							width = newX-pivotX;
+							height = newY-pivotY;
+							upperX = pivotX;
+							upperY = pivotY;
+//							Graphics2D g2 = (Graphics2D) getGraphics();
+//							g2.fillOval(pivotX, pivotY, newX-pivotX, newY-pivotY);
+						}	
+							Graphics2D g2 = (Graphics2D) getGraphics();
+							g2.fillOval(upperX,upperY, width, height);
 						
 					}
 					else if(image.isSelected()){
@@ -142,13 +195,14 @@ public class NetpaintGUI extends JFrame{
 					}
 					else if(line.isSelected()){
 						
-						
+							Graphics2D g2 = (Graphics2D) getGraphics();
+							Line2D.Double line = new Line2D.Double(pivotX, pivotY, newX, newY);
+							g2.draw(line);
 					}
 					else{
 						System.out.println("Select a Shape first.");
 					}
 				}
-				
 				oldX = newX;
 				oldY = newY;
 			}
